3debd1e69484abd899db5f8ab0b2841fd70514d1
Femi Akinyemi <akinfemi46@gmail.com>
2020-11-12 12:13:09 +0100

    Implemented Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+import Navbar from "./Components/Navbar";
 import Sidebar from "./Components/Sidebar";
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom'
+import About from "./Components/About";
+import Project from "./Components/Project";
+import Resume from "./Components/Resume";
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <div className="container ">
         <div className="row">
@@ -10,11 +17,31 @@ function App() {
             {/* <Sideb<div className="app_sidebar"></div> */}
             <Sidebar/>
           </div>
-          <div className="col-lg-9 app_main-content"></div>
+          <div className="col-lg-9 app_main-content">
+            {/* navbar */}
+            <Navbar/>
+            <Switch>
+
+            
+            <Route exact path="/">
+            <About/>
+            </Route>
+            <Route path="/projects">
+              <Project/>
+            </Route>
+            <Route  path="/resume">
+              <Resume/>
+            </Route>
+            <Route>
+              <Redirect to="/" />
+            </Route>
+            </Switch>
+          </div>
         </div>
       </div>
       
     </div>
+    </Router>
   );
 }
 
---
+++ src/Components/Project.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Project() {
+  return (
+    <div>
+      <h3>Project Page</h3>
+    </div>
+  )
+}
